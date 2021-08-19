5129b9a9225fc642dde93a24db4f4146420d0326
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-08-19 14:54:13 -0400

    Update postb.php

--- src/postb.php
+++ src/postb.php
@@ -1,4 +1,5 @@
 <?php
+$old = file_get_contents("posts.txt");
 $postorig = $_GET["p"];
 $postfix1 = str_replace("<", "", $postorig);
 $postfix2 = str_replace(">", "", $postfix1);
@@ -6,7 +7,7 @@ $postc = $postfix2;
 if (is_file("posts.txt")) {False;} else {
     fopen("posts.txt", "w");
 }
-$postsf = fopen("posts.txt", "a");
-fwrite($postsf, $postc . "<br><br>");
+$postsf = fopen("posts.txt", "w");
+fwrite($postsf, $postc . "<br><br>" . $old);
 echo '<script>window.location.href = "/";</script>';
 ?>
